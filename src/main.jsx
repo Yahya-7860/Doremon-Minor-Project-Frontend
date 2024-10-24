b0e725e96a9ad5f98389d14f69e76262dffe73b0
Yahya Ansari <sayahya.ansari@gmail.com>
2024-10-24 21:48:12 +0530

    routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,31 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
-// import Login_signup from './page/Login_signup.jsx'
-import WelcomePage from './page/WelcomePage.jsx'
+import Login_signup from "./page/Login_signup.jsx"
+import WelcomePage from "./page/WelcomePage.jsx"
+import GamePage from "./page/GamePage.jsx"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login_signup />,
+  },
+  {
+    path: "/welcome",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/game",
+    element: <GamePage />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    {/* <Login_signup /> */}
-    {/* <WelcomePage /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/page/WelcomePage.jsx
+++ src/page/WelcomePage.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "../CSS Folder/welcomePage.module.css"
+import { useNavigate } from "react-router-dom";
 
 function WelcomePage() {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/game')
+    }
+
     return <>
         <div className={styles.main_container}>
             <div className={styles.text_box}>
                 <div className={styles.textDiv}>
                     <h1 className={styles.welcomeText}>Welcome to Doremon Escape Game</h1>
-                    <button className={styles.welcomeBtn}>Let's Play</button>
+                    <button onClick={handleClick} className={styles.welcomeBtn}>Let's Play</button>
                 </div>
             </div>
         </div>
