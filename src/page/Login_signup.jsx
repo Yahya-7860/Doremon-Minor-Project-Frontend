2717deaea4fd3f4f1a0d519010df6952157176dc
Yahya Ansari <sayahya.ansari@gmail.com>
2024-11-01 22:04:35 +0530

    add login register api integration

--- src/page/Login_signup.jsx
+++ src/page/Login_signup.jsx
@@ -1,40 +1,128 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "../CSS Folder/login_signup.module.css"
+import { useNavigate } from "react-router-dom";
+import ConfettiExplosion from 'react-confetti-explosion'
 
 function Login_signup() {
+    const navigate = useNavigate();
+    const [input, setInput] = useState({
+        RegUsername: '',
+        RegPassword: '',
+        LogUsername: '',
+        LogPassword: ''
+    });
+
+    const [state, setState] = useState({
+        isExist: false,
+        confTrue: false,
+        invalidUser: false,
+        wrongPass: false
+    });
+
     const containerRef = useRef(null)
     const handleSignupClick = () => {
         containerRef.current.classList.add(styles.sign_up_mode);
     };
-
     const handleSigninClick = () => {
         containerRef.current.classList.remove(styles.sign_up_mode);
     };
+    const handle_Register_Submit = async (e) => {
+        e.preventDefault();
+        setState((pre) => ({ ...pre, confTrue: false }))
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: input.RegUsername, password: input.RegPassword }),
+        }
+
+        await fetch("http://localhost:3000/user/register", options)
+            .then((res) => res.json())
+            .then((data) => {
+                const token = data.token;
+                localStorage.setItem('token', token);
+                if (data.message === 'exist') {
+                    setState((pre) => ({ ...pre, isExist: true }))
+                    return;
+                }
+                setState((pre) => ({ ...pre, confTrue: true }))
+                // navigate('/welcome')
+
+            })
+            .catch((err) => console.error(err))
+
+    }
+
+    const handle_login_submit = async (e) => {
+        e.preventDefault();
+        setState((pre) => ({ ...pre, confTrue: false }))
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: input.LogUsername, password: input.LogPassword }),
+        }
+
+        await fetch("http://localhost:3000/user/login", options)
+            .then((res) => res.json())
+            .then((data) => {
+                const token = data.token;
+                localStorage.setItem('token', token)
+                if (data.message === "invalid user") {
+                    setState((pre) => ({ ...pre, invalidUser: true }))
+                    return;
+                }
+                else if (data.message === "invalid password") {
+                    setState((pre) => ({ ...pre, wrongPass: true }))
+                    return;
+                }
+                navigate('/welcome')
+
+            })
+            .catch((err) => console.log(err))
+    }
     return <>
         <div ref={containerRef} className={styles.container}>
             <div className={styles.forms_container}>
                 <div className={styles.signin_signup}>
-                    <form className={styles.sign_in_form}>
+                    {/* login */}
+                    <form className={styles.sign_in_form} onSubmit={handle_login_submit}>
                         <h2 className={styles.title}>Sign in</h2>
                         <div className={styles.input_field}>
                             <i className={`${styles.fas} ${styles.fa_user}`}></i>
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" value={input.LogUsername} onChange={(e) => {
+                                setInput((pre) => ({ ...pre, LogUsername: e.target.value }));
+                                setState((pre) => ({ ...pre, invalidUser: false }))
+
+                            }} />
                         </div>
+                        {state.invalidUser && <p className={styles.isInvalid}>Invalid Username</p>}
                         <div className={styles.input_field}>
                             <i className={`${styles.fas} ${styles.fa_lock}`}></i>
-                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Password" value={input.LogPassword} onChange={(e) => {
+                                setInput((pre) => ({ ...pre, LogPassword: e.target.value }))
+                                setState((pre) => ({ ...pre, wrongPass: false }))
+                            }} />
                         </div>
+                        {state.wrongPass && <p className={styles.isInvalid}>Wrong Password</p>}
                         <input type="submit" value="Login" className={`${styles.btn} ${styles.solid}`} />
                     </form>
-                    <form className={styles.sign_up_form}>
+                    {/* register */}
+                    <form className={styles.sign_up_form} onSubmit={handle_Register_Submit} >
                         <h2 className={styles.title}>Sign up</h2>
                         <div className={styles.input_field}>
                             <i className={`${styles.fas} ${styles.fa_user}`}></i>
-                            <input type="text" placeholder="Username" />
+                            <input type="text" placeholder="Username" value={input.RegUsername} onChange={(e) => {
+                                setInput((pre) => ({ ...pre, RegUsername: e.target.value }));
+                                setState((pre) => ({ ...pre, isExist: false }))
+                            }} />
                         </div>
+                        {state.isExist && <p className={styles.isExist}>Username already exist</p>}
                         <div className={styles.input_field}>
                             <i className={`${styles.fas} ${styles.fa_lock}`}></i>
-                            <input type="password" placeholder="Password" />
+                            <input type="password" placeholder="Password" value={input.RegPassword} onChange={(e) => setInput((pre) => ({ ...pre, RegPassword: e.target.value }))} />
                         </div>
                         <input type="submit" className={styles.btn} value="Sign up" />
                     </form>
@@ -67,6 +155,7 @@ function Login_signup() {
                     <img src="/images/SignupDora.png" className={styles.image} />
                 </div>
             </div>
+            {state.confTrue && <ConfettiExplosion particleCount={400} zIndex={10} width={3000} />}
         </div>
 
     </>;
